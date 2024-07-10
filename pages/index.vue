b9d187ef4ec9fee8734ccbbe0c4da19f1e7e49c7
Bureaupixel <bram.overstijns@gmail.com>
2024-07-10 21:45:46 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    <p class="pageTitle">Currating and visualizing a Library</p>
+    <p class="pageTitle">Curating and visualizing a Library</p>
     <p class="text-2xl">
         This example starts from a yearly currated list of books from the library. The dataset is based on the dataset of LIMO and accessible as a JSON file.
     </p>
